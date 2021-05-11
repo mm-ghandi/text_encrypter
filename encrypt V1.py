506b1acd6c9ea569ebd249e6df297226a8b8aa7e
Amirmohammad Ghazi dizaji <72940447+amirghazi87@users.noreply.github.com>
2021-05-11 13:32:03 +0430

    Rewrite
    I don't add new future
    but rewrite with better code (:
    and smaller
    formatted with pep8

--- encrypt V1.py
+++ encrypt V1.py
@@ -1,62 +1,51 @@
-letters = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'," "]
-originallet = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'," "]
-#,'(',')','_',"=","+","-",",","."
-def gettext():
-  text = input("enter yout text: ")
-  password = input("enter a password: ")
-  num = 0
-  for i in password :
-    indexed = originallet.index(i)
-    num += indexed
-  for i in range(num):
-    fakenum = letters[0]
-    letters.remove(fakenum)
-    letters.append(fakenum)
-  finaltext = ""
-  for i in text :
-    number = originallet.index(i)
-    finaltext += letters[number]
-  print(finaltext)
+letters = ["a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', " "]
+originallet = ["a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+               'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', " "]
 
-def getpass():
-  entext = input("enter the encrypted text: ")
-  enpass = input("enter the password: ")
-  num = 0
-  for i in enpass :
-    indexed = originallet.index(i)
-    num += indexed
-  for i in range(num):
-    fakenum = letters[0]
-    letters.remove(fakenum)
-    letters.append(fakenum)
-  textindex = []
-  for i in entext:
-    heretext = letters.index(i)
-    textindex.append(heretext)  
-  for i in letters :
-    if i == "a" :
-      break
-    removepoint = [i]
-  for i in removepoint :
-    letters.remove(i)
-    letters.append(removepoint)
-  finaltext = ""
-  for i in textindex :
-    fine = originallet[i]
-    
-    finaltext += fine
-  print(finaltext)
 
-ok = "y"
-while ok == "y" :  
-  choice = input("encrypt or decrypte (e/d)")
+def get_inputs():
+    text = input("Enter your text: ")
+    password = input("Enter your password: ")
+    return [text, password]
 
-  if choice == "e" :
-    gettext()
-  elif choice == 'd' :
-    getpass()
-  else :
-    print("its a wrong input")
 
-  ok = input("do you want to continue?? (y/n)")
-  letters = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'," "]
+def encrypt(letters):
+    text, password = get_inputs()
+    num = 0
+    for i in password:
+        num += originallet.index(i)
+    for i in range(num):
+        letters = letters[1:] + list(letters[0])
+    result = ""
+    for i in text:
+        result += letters[originallet.index(i)]
+    return result
+
+
+def decrypte(letters):
+    text, password = get_inputs()
+    num = 0
+    for i in password:
+        num += originallet.index(i)
+    for i in range(num):
+        letters = letters[1:] + list(letters[0])
+    result = ""
+    for i in text:
+        result += originallet[letters.index(i)]
+    return result
+
+
+if __name__ == "__main__":
+    ok = "y"
+    while ok == "y":
+          choice = input("Encrypt or decrypte (e/d)")
+          if choice == "e":
+            print(encrypt(letters))
+          elif choice == 'd':
+            print(decrypte(letters))
+          else:
+            print("it\'s a wrong input")
+          ok = input("Continue ? (y/n)")
+          letters = ["a", "b", 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                       's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', " "]
